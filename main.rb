b5e944e8076cf739e3a3dd9681d4cf5f323ffc18
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-06 11:57:31 +0300

    Added bubble_sort basic method

---
+++ main.rb
@@ -0,0 +1,35 @@
+def bubble_sort(array)
+
+  # loop the array and check the current element if is bigger then the next element, in case is bigger we swap them
+  iteration = 0
+
+  loop do
+    # we make sorted true by default and in case we have any swap we will make it false
+    sorted = true
+
+    array.each_with_index do |element, index|
+      nextElement = array[index + 1]
+      
+      break if !nextElement
+
+      if element > nextElement
+        sorted = false
+
+        array[index] = nextElement
+        array[index + 1] = element
+      end
+    end
+
+    # when we loop the array we need to know if anything was swaped, in order to do this we will initialize a variable outside of the loop and if there is any swap then we change the variable
+
+    # after we loop all the array, if we had any swaps during the loop then we should start looping again because we don`t know if the array is sorted
+    
+    # if the sorted remains true after a iteration of the array then the array is sorted and we can break the loop
+
+    break if sorted == true
+  end
+
+  p array
+end
+
+bubble_sort([756545, 1, 5, 2, -20, 56, 999, 34, 546, 65464])
---
+++ s
@@ -0,0 +1,15 @@
+
+[1mFrom:[0m C:/Users/aurasjobs/Desktop/Microverse/microverse-bubble-sort/main.rb:23 Object#bubble_sort:
+
+    [1;34m18[0m:         array[index] = nextElement
+    [1;34m19[0m:         array[index + [1;34m1[0m] = element
+    [1;34m20[0m:       [32mend[0m
+    [1;34m21[0m:     [32mend[0m
+    [1;34m22[0m: 
+ => [1;34m23[0m:     binding.pry
+    [1;34m24[0m:     [1;34m# when we loop the array we need to know if anything was swaped, in order to do this we will initialize a variable outside of the loop and if there is any swap then we change the variable[0m
+    [1;34m25[0m: 
+    [1;34m26[0m:     [1;34m# after we loop all the array, if we had any swaps during the loop then we should start looping again because we don`t know if the array is sorted[0m
+    [1;34m27[0m:     
+    [1;34m28[0m:     [1;34m# if the sorted remains true after a iteration of the array then the array is sorted and we can break the loop[0m
+
