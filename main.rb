084ed910aaa035b0b2a15c86bf8fd2a6f9e3af6d
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-06 12:36:15 +0300

    Fixed rubocop errors

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 def bubble_sort(array)
-
   # Count how many times did we iterate the array
   iteration = 0
 
@@ -8,22 +7,23 @@ def bubble_sort(array)
     sorted = true
     iteration += 1
 
-    # With each loop we iterate all the array elements to check if we need to swap any, if we swap at least one element we set the sorted to false
+    # With each loop we iterate all the array elements to check if we need to swap any
+    # if we swap at least one element we set the sorted to false
     array.each_with_index do |element, index|
-      nextElement = array[index + 1]
-      
-      
+      next_element = array[index + 1]
       # With each full iteration one more last element is already set
       # When we have n iteration, n elements from the end of the array are already set
       # We also break if the next element does not exist
-      break if !nextElement && iteration > array.length - index - 1
+      break if !next_element && iteration > array.length - index - 1
 
-      if element > nextElement
+      if element > next_element
         sorted = false
 
-        array[index] = nextElement
+        array[index] = next_element
         array[index + 1] = element
+        next
       end
+      next
     end
 
     # If after a full iteration the sorted is still true we can break the loop
@@ -33,37 +33,35 @@ def bubble_sort(array)
   array
 end
 
-p bubble_sort([756545, 1, 5, 2, -20, 56, 999, 34, 546, 65464])
-
+p bubble_sort([45, 1, 5, 2, -20, 56, 999, 34, 46, 64])
 
 def bubble_sort_by(array)
+  iteration = 0
 
-    iteration = 0
-  
-    loop do
-      # we make sorted true by default and in case we have any swap we will make it false
-      sorted = true
-      iteration += 1
-  
-      array.each_with_index do |element, index|
-        nextElement = array[index + 1]
-        
-        break if !nextElement && iteration > array.length - index - 1
-  
-        if yield(element, nextElement).positive?
-          sorted = false
-  
-          array[index] = nextElement
-          array[index + 1] = element
-        end
+  loop do
+    # we make sorted true by default and in case we have any swap we will make it false
+    sorted = true
+    iteration += 1
+
+    array.each_with_index do |element, index|
+      next_element = array[index + 1]
+      break if !next_element && iteration > array.length - index - 1
+
+      if yield(element, next_element).positive?
+        sorted = false
+
+        array[index] = next_element
+        array[index + 1] = element
+        next
       end
-  
-      break if sorted == true
+      next
     end
-  
-    array
+
+    break if sorted == true
   end
 
+  array
+end
 
-  testıng = bubble_sort_by(["hi","hello","hey", "hellog again", "h", "hm"]) { |left,right| left.length - right.length }
-p testıng
+result = bubble_sort_by(['hi', 'hello', 'hey', 'hellog again', 'h', 'hm']) { |left, right| left.length - right.length }
+p result
