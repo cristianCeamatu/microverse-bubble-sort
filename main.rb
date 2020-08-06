2fa0311622c82bda7d1d7e9681c045048bd1e4ca
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-06 12:25:12 +0300

    Small changes on the comments

--- main.rb
+++ main.rb
@@ -1,16 +1,21 @@
 def bubble_sort(array)
 
-  # loop the array and check the current element if is bigger then the next element, in case is bigger we swap them
+  # Count how many times did we iterate the array
   iteration = 0
 
   loop do
-    # we make sorted true by default and in case we have any swap we will make it false
+    # We make sorted true by default and in case we have any swap we will make it false
     sorted = true
     iteration += 1
 
+    # With each loop we iterate all the array elements to check if we need to swap any, if we swap at least one element we set the sorted to false
     array.each_with_index do |element, index|
       nextElement = array[index + 1]
       
+      
+      # With each full iteration one more last element is already set
+      # When we have n iteration, n elements from the end of the array are already set
+      # We also break if the next element does not exist
       break if !nextElement && iteration > array.length - index - 1
 
       if element > nextElement
@@ -21,12 +26,7 @@ def bubble_sort(array)
       end
     end
 
-    # when we loop the array we need to know if anything was swaped, in order to do this we will initialize a variable outside of the loop and if there is any swap then we change the variable
-
-    # after we loop all the array, if we had any swaps during the loop then we should start looping again because we don`t know if the array is sorted
-    
-    # if the sorted remains true after a iteration of the array then the array is sorted and we can break the loop
-
+    # If after a full iteration the sorted is still true we can break the loop
     break if sorted == true
   end
 
@@ -38,7 +38,6 @@ p bubble_sort([756545, 1, 5, 2, -20, 56, 999, 34, 546, 65464])
 
 def bubble_sort_by(array)
 
-    # loop the array and check the current element if is bigger then the next element, in case is bigger we swap them
     iteration = 0
   
     loop do
@@ -59,17 +58,12 @@ def bubble_sort_by(array)
         end
       end
   
-      # when we loop the array we need to know if anything was swaped, in order to do this we will initialize a variable outside of the loop and if there is any swap then we change the variable
-  
-      # after we loop all the array, if we had any swaps during the loop then we should start looping again because we don`t know if the array is sorted
-      
-      # if the sorted remains true after a iteration of the array then the array is sorted and we can break the loop
-  
       break if sorted == true
     end
   
     array
   end
 
-testıng = bubble_sort_by(["hi","hello","hey", "hellog again", "h", "hm"]) { |left,right| left.length - right.length }
+
+  testıng = bubble_sort_by(["hi","hello","hey", "hellog again", "h", "hm"]) { |left,right| left.length - right.length }
 p testıng
